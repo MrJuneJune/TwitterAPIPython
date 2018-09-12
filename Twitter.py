9cfe437777f9ae4dd89e9ed4810ca1d95f8fb669
Your Name <junepark95@hotmail.com>
2018-09-11 20:43:42 -0400

    Added function that collects Donald Trump Tweets every 5 minutes and save the tweet as json file

--- Twitter.py
+++ Twitter.py
@@ -6,12 +6,21 @@ Created on Sun Sep  2 19:16:13 2018
 
 Title: Connecting to twitter API to pull tweets.
 """
-
+#Tweepy lib for connecting to twitter API
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
 from tweepy import Cursor
+
+#To set schedule.
+import schedule
+import time
+from datetime import datetime
+
+#To create library
+import os
+import json
 
 #Reading Consumer and Access Keys 
 Keys = open("Consumer_Access_Codes.txt", "r").read().split('\n')
@@ -31,37 +40,52 @@ class TwitterAuthenticator():
 
 # Pulling tweets from timeline.
 class TwitterClient():
+
     def __init__(self, tweeter=None):
         #Authenticating Twitter API.
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
         
+        #Creating folder to save files.
+        dir_path = os.path.dirname(os.path.realpath("Twitter.py"))
+        directory = dir_path + '/' + tweeter
+        if not os.path.exists(directory):
+             os.makedirs(directory)
+        
+        self.directory = directory
+
         #Specifying tweeter user.
         self.tweeter = tweeter
 
     def get_user_timeline_tweets(self, num_tweets):
-        #Saving Tweets in a string as a list.
-        tweets=[]
+
+        #Saving Tweets in a string as a file.
+        directory_timeline = self.directory + "/timeline.json"
+        f = open(directory_timeline,"a")
         for tweet in Cursor(self.twitter_client.user_timeline, id=self.tweeter).items(num_tweets):
-            tweets.append(tweet)
-        print(tweets)
-        return(tweets)
+            f.write(json.dumps(tweet._json))
 
     def get_user_friendlist(self,num_friends):
         #Saving friends in a string as a list.
-        friends=[]
+        directory_timeline = self.directory + "/friends.json"
+        f = open(directory_timeline,"a")
         for friend in Cursor(self.twitter_client.friends, id=self.tweeter).items(num_friends):
-            friends.append(friend)
-        return(friends)
+            f.write(json.dumps(friend._json))
 
     def get_user_followers(self,num_followers):
         #Saving followers in a string as a list.
-        followers = []
+        directory_timeline = self.directory + "/followers.json"
+        f = open(directory_timeline,"a")
         for follower in Cursor(self.twitter_client.followers, id=self.tweeter).items(num_followers):
-            followers.append(follower)
-        print(followers)
-        return(followers)
-
+            f.write(json.dumps(follower._json))
+    
+    def get_user_homepage_tweets(self,num_tweets):
+        directory_timeline = self.directory + "/homepage.json"
+        f = open(directory_timeline,"a")
+        for htweet in Cursor(self.twitter_client.home_timeline, id=self.tweeter).items(num_tweets):
+            f.write(json.dumps(htweet._json))
+        
+            
 class TweetListener(StreamListener):
 
     #Simple class that listens to tweets.
@@ -100,17 +124,31 @@ class TwitterStreamer():
         #Pull Stream of tweets with hashtags.
         stream.filter(track=hashtags)
 
+def collect_donald_tweet():
+    print('started! at ' + str(datetime.now()))
+    twitter_client = TwitterClient('realDonaldTrump')
+    twitter_client.get_user_timeline_tweets(1)
+    print('Collected! at ' + str(datetime.now()))
+
 
 if __name__ == "__main__":
+
+    schedule.every(5).minutes.do(collect_donald_tweet)
+    
+    while True:
+        try:
+            schedule.run_pending()
+            time.sleep(2)
+        except:
+            pass
+
     #hash_tag_list=["donald trump"]
     #fetched_tweets_filename = "tweets.json"
-    
-    test = open("test.txt",'w')
-    twitter_client=TwitterClient('Godofshinobi')
-    twitter_client.get_user_timeline_tweets(1)
-    twitter_client.get_user_friendlist(1)
-    twitter_client.get_user_followers(1)
-    test.close()
+
+    #twitter_client=TwitterClient('pycon')
+    #twitter_client.get_user_timeline_tweets(1)
+    #twitter_client.get_user_friendlist(1)
+    #twitter_client.get_user_followers(1)
 
     #twitter_streamer = TwitterStreamer()
     #twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
